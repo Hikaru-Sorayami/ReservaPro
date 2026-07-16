da147b11cd317f50f96552ec9ca62a1be8c3081d
Hikaru-Sorayami <lu.castrot@duocuc.cl>
2026-07-15 20:14:57 -0400

    Modificacion Evaluacion
    Modificacion añadiendo un test al ser una que me pidieron dentro de la evaluacion

--- usuario/src/test/java/com/example/reservapro/UsuarioServiceTest.java
+++ usuario/src/test/java/com/example/reservapro/UsuarioServiceTest.java
@@ -1,5 +1,7 @@
-﻿package com.example.reservapro;
+package com.example.reservapro;
 
+import com.example.reservapro.Model.EstadoUsuario;
+import com.example.reservapro.Model.RolUsuario;
 import com.example.reservapro.Model.Usuario;
 import com.example.reservapro.repository.UsuarioRepository;
 import com.example.reservapro.service.UsuarioService;
@@ -10,8 +12,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 
-
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -35,5 +36,34 @@ class UsuarioServiceTest {
         assertSame(entidad, resultado);
         verify(repository).save(entidad);
     }
+
+
+    @Test
+    void guardarUsuarioConEmailExistenteDebeLanzarExcepcion() {
+
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Luciano");
+        usuario.setApellido("Castro");
+        usuario.setEmail("luciano@gmail.com");
+        usuario.setPassword("123456");
+        usuario.setTelefono("987654321");
+        usuario.setDireccion("Av. Libertador 123");
+        usuario.setRol(RolUsuario.CLIENTE);
+        usuario.setEstado(EstadoUsuario.ACTIVO);
+
+        when(repository.existsByEmail("luciano@gmail.com"))
+                .thenReturn(true);
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> service.guardar(usuario)
+        );
+
+        System.out.println(exception.getMessage());
+
+        assertEquals("Ya existe un usuario con ese email", exception.getMessage());
+    }
+
+
 }
 
